1cf00c584e2028c99c41f90ce78ca76a69f786c6
lucascmreis <reislcr.rj@gmail.com>
2020-04-19 20:53:07 -0300

    front-end app

--- backend/src/controllers/ProfileController.js
+++ backend/src/controllers/ProfileController.js
@@ -1,12 +1,13 @@
-const connection = require('../database/connection');
+const connection = require('../database/connection')
 
 module.exports = {
-    async index(request, response){
-        const ong_id = request.headers.authorization;
-        const incidents = await connection('incidents')
-        .where('ong_id', ong_id)
-        .select('*');
+  async index(request, response) {
+    const ong_id = request.headers.authorization
 
-        return response.json(incidents);
-    }
+    const incidents = await connection('incidents')
+      .where('ong_id', ong_id)
+      .select('*')
+
+    return response.json(incidents)
+  },
 }
